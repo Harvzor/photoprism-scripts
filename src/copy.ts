12f00a8a075c703b15ebfa757f4f6f0306fca8c6
harvzor <harveyjwilliams@aol.co.uk>
2022-08-06 12:51:27 +0200

    Improved folder structure, refactored, added Code debugging, added archived image moving.

--- src/copy.ts
+++ src/copy.ts
@@ -1,6 +1,7 @@
 import * as yaml from 'js-yaml'
 import * as fs from 'fs'
 import * as path from 'path'
+import { SidecarFile } from './types/SidecarFile'
 
 // const originalsPath = '/media/harvey/data/Images/Life/main/'
 // const storagePath = '/media/harvey/data/Images/PhotoPrism/storage/'
@@ -10,50 +11,12 @@ const storagePath = '/media/harvey/data/Dev/Tech/Node/photoprism-scripts/data/st
 
 const sidecarPath = path.join(storagePath, 'sidecar/')
 
-/*
-TakenAt: 2003-07-11T14:08:33Z
-TakenSrc: meta
-UID: pra8dk339xe61swy
-Type: video
-Title: Seashore / 2003
-OriginalName: random-video/Bike/Vietnam 2017/desktop maybe/test/VIDO0018
-Year: 2003
-Month: 7
-Day: 11
-Quality: 3
-Favourite: true
-Private: true
-Details:
-  Keywords: bike, brown, desktop, maybe, random, seashore, test, video, vietnam
-CreatedAt: 2022-04-12T14:28:51Z
-UpdatedAt: 2022-08-05T18:06:59.585384018Z
-DeletedAt: 2022-08-05T18:06:59.585384018Z
-*/
-export interface SidecarFile {
-    TakenAt:      Date
-    TakenSrc:     string
-    UID:          string
-    Type:         string
-    Title:        string
-    OriginalName: string
-    Year:         number
-    Month:        number
-    Day:          number
-    Quality:      number
-    Favourite:    boolean
-    Private:      boolean
-    Details:      SideCarFileDetails
-    CreatedAt:    Date
-    UpdatedAt:    Date
-    DeletedAt:    Date
-}
-
-export interface SideCarFileDetails {
-    Keywords: string
-}
-
-const search = async function(folder: string): Promise<string[]> {
-    const paths: string[] = []
+/**
+ * Find YAML files in the sidecar dir.
+ * @param  {string} folder
+ */
+const recursiveSearchSidecar = async function(folder: string): Promise<string[]> {
+    let paths: string[] = []
 
     try {
         const filesOrFolderNames = await fs.promises.readdir(folder)
@@ -63,10 +26,9 @@ const search = async function(folder: string): Promise<string[]> {
 
             if (stat.isFile()) {
                 if (path.extname(fileOrFolderName) === '.yml')
-                    if (matches(fileOrFolderPath))
-                        paths.push(fileOrFolderPath)
+                    paths.push(fileOrFolderPath)
             } else if (stat.isDirectory()) {
-                await search(fileOrFolderPath)
+                paths = paths.concat(await recursiveSearchSidecar(fileOrFolderPath))
             }
         }
     } catch (err) {
@@ -76,15 +38,11 @@ const search = async function(folder: string): Promise<string[]> {
     return paths
 }
 
-const matches = function(yamlFilePath: string): boolean {
+const readYamlFile = function(yamlFilePath: string): SidecarFile{
     try {
-        const doc = yaml.load(fs.readFileSync(yamlFilePath, 'utf8')) as SidecarFile
+        const doc = yaml.load(fs.readFileSync(yamlFilePath, 'utf8')) as SidecarFile 
 
-        if (doc.Private) {
-            return true
-        }
-
-        return false
+        return doc
     } catch (e) {
         console.log(e)
 
@@ -99,12 +57,8 @@ const findImagePaths = async function(paths: string[]): Promise<string[]> {
         if (!yamlPath.startsWith(sidecarPath))
             throw 'path doens\'t start correctly'
 
-        console.log(yamlPath)
-
         // This file doesn't exist.
         const imageLocationPath = path.join(originalsPath, yamlPath.substring(sidecarPath.length))
-
-        console.log(imageLocationPath)
 
         // But the file should be in this dir.
         const imageLocationDir = path.dirname(imageLocationPath)
@@ -115,8 +69,6 @@ const findImagePaths = async function(paths: string[]): Promise<string[]> {
             if (stat.isDirectory())
                 continue
 
-            // console.log(yamlPath, potentialMatch)
-
             // If the file name matches.
             if (path.basename(yamlPath).split('.')[0] === potentialMatch.split('.')[0]) {
                 result.push(path.join(imageLocationDir, potentialMatch))
@@ -126,9 +78,12 @@ const findImagePaths = async function(paths: string[]): Promise<string[]> {
 
     return result
 }
-
-const moveImages = async function(imagePaths: string[]) {
-    const targetDir = path.join(originalsPath, 'private')
+/**
+ * @param  {string[]} imagePaths Full image paths.
+ * @param  {string} targetFolder The name of the folder which the images should be moved to.
+ */
+const moveImages = async function(imagePaths: string[], targetFolder: string) {
+    const targetDir = path.join(originalsPath, targetFolder)
 
     if (!fs.existsSync(targetDir))
         await fs.promises.mkdir(targetDir)
@@ -142,8 +97,20 @@ const moveImages = async function(imagePaths: string[]) {
     }
 }
 
-const yamlPaths = await search(sidecarPath)
+const yamlPaths = await recursiveSearchSidecar(sidecarPath)
 
-const imagePaths = await findImagePaths(yamlPaths)
+console.log(yamlPaths)
 
-moveImages(imagePaths)
+const privateYamlPaths = yamlPaths
+    .filter(x => readYamlFile(x).Private)
+console.log('private', privateYamlPaths)
+const privateImagePaths = await findImagePaths(privateYamlPaths)
+console.log(privateImagePaths)
+moveImages(privateImagePaths, 'private')
+
+const archivedYamlPaths = yamlPaths
+    .filter(x => readYamlFile(x).DeletedAt !== undefined)
+console.log('archived', archivedYamlPaths)
+const archivedImagePaths = await findImagePaths(archivedYamlPaths)
+console.log(archivedImagePaths)
+moveImages(archivedImagePaths, 'archived')
---
+++ src/types/SidecarFile.ts
@@ -0,0 +1,40 @@
+import { SideCarFileDetails } from "./SideCarFileDetails";
+
+/*
+TakenAt: 2003-07-11T14:08:33Z
+TakenSrc: meta
+UID: pra8dk339xe61swy
+Type: video
+Title: Seashore / 2003
+OriginalName: random-video/Bike/Vietnam 2017/desktop maybe/test/VIDO0018
+Year: 2003
+Month: 7
+Day: 11
+Quality: 3
+Favourite: true
+Private: true
+Details:
+  Keywords: bike, brown, desktop, maybe, random, seashore, test, video, vietnam
+CreatedAt: 2022-04-12T14:28:51Z
+UpdatedAt: 2022-08-05T18:06:59.585384018Z
+DeletedAt: 2022-08-05T18:06:59.585384018Z
+*/
+
+export interface SidecarFile {
+    TakenAt: Date;
+    TakenSrc: string;
+    UID: string;
+    Type: string;
+    Title: string;
+    OriginalName: string;
+    Year: number;
+    Month: number;
+    Day: number;
+    Quality: number;
+    Favourite: boolean;
+    Private: boolean;
+    Details: SideCarFileDetails;
+    CreatedAt: Date;
+    UpdatedAt: Date;
+    DeletedAt: Date;
+}
