e3b304e079df2607cb7ffa368e279000475814cd
harvzor <harveyjwilliams@aol.co.uk>
2022-08-09 15:02:35 +0200

    Added a UI which checks what you want to do.

--- src/copy.ts
+++ src/copy.ts
@@ -1,13 +1,14 @@
 import * as yaml from 'js-yaml'
 import * as fs from 'fs'
 import * as path from 'path'
+import inquirer from 'inquirer'
 import { SidecarFile } from './types/SidecarFile'
 
-const originalsPath = '/media/harvey/data/Images/Life/main/'
-const storagePath = '/media/harvey/data/Images/PhotoPrism/storage/'
+// const originalsPath = '/media/harvey/data/Images/Life/main/'
+// const storagePath = '/media/harvey/data/Images/PhotoPrism/storage/'
 
-// const originalsPath = '/media/harvey/data/Dev/Tech/Node/photoprism-scripts/photoprism-test/data/originals/'
-// const storagePath = '/media/harvey/data/Dev/Tech/Node/photoprism-scripts/photoprism-test/data/storage/'
+const originalsPath = '/media/harvey/data/Dev/Tech/Node/photoprism-scripts/photoprism-test/data/originals/'
+const storagePath = '/media/harvey/data/Dev/Tech/Node/photoprism-scripts/photoprism-test/data/storage/'
 
 const sidecarPath = path.join(storagePath, 'sidecar/')
 
@@ -67,26 +68,34 @@ const findImagePath = async function(yamlPath: string): Promise<string[]> {
         )
     )
 
-    const potentialMatches = await fs.promises.readdir(imageLocationDir)
-    let matchesFound = 0
-    for (const potentialMatch of potentialMatches) {
-        const stat = await fs.promises.stat(path.join(imageLocationDir, potentialMatch))
+    try {
+        // Check if the target dir exists.
+        await fs.promises.access(imageLocationDir)
 
-        if (stat.isDirectory())
-            continue
+        const potentialMatches = await fs.promises.readdir(imageLocationDir)
+        let matchesFound = 0
+        for (const potentialMatch of potentialMatches) {
+            const stat = await fs.promises.stat(path.join(imageLocationDir, potentialMatch))
 
-        // If the file name matches.
-        // Could also match multiple times if the image is a burst, such as '20210717_163906_1BF7A639.00002.jpg'.
-        // If a burst is detected, there will only be one YAML file called ''20210717_163906_1BF7A639.yml'.
-        if (path.basename(yamlPath).split('.')[0] === potentialMatch.split('.')[0]) {
-            result.push(path.join(imageLocationDir, potentialMatch))
-            matchesFound++
+            if (stat.isDirectory())
+                continue
+
+            // If the file name matches.
+            // Could also match multiple times if the image is a burst, such as '20210717_163906_1BF7A639.00002.jpg'.
+            // If a burst is detected, there will only be one YAML file called ''20210717_163906_1BF7A639.yml'.
+            if (path.basename(yamlPath).split('.')[0] === potentialMatch.split('.')[0]) {
+                result.push(path.join(imageLocationDir, potentialMatch))
+                matchesFound++
+            }
         }
-    }
 
-    // Somehow I have YAML files which are orphaned.
-    if (matchesFound == 0)
-        console.log(`No image found for ${yamlPath}`)
+        // Somehow I have YAML files which are orphaned.
+        if (matchesFound == 0)
+            console.log(`No image found for ${yamlPath}`)
+    } catch {
+        // Folder does not exist.
+        // YAMLs are orphans.
+    }
 
     return result
 }
@@ -106,6 +115,18 @@ const findImagePaths = async function(yamlPaths: string[]): Promise<string[]> {
  * @param  {string} targetFolder The name of the folder which the images should be moved to.
  */
 const moveImages = async function(imagePaths: string[], targetFolder: string) {
+    const choices = [
+        'Move',
+        'Don\'t move',
+        'Move all (auto)',
+    ]
+    let auto = false
+
+    const move = function(oldImagePath: string, newImagePath: string) {
+        console.log(`Moving media file from ${oldImagePath} to ${newImagePath}`)
+        // await fs.promises.rename(imagePath, newImagePath)
+    }
+
     const targetDir = path.join(originalsPath, targetFolder)
 
     if (!fs.existsSync(targetDir)) {
@@ -113,20 +134,53 @@ const moveImages = async function(imagePaths: string[], targetFolder: string) {
         await fs.promises.mkdir(targetDir)
     }
 
-    for (const imagePath of imagePaths) {
+    const imagePathsThatNeedMoving = imagePaths
         // No need to move as the file is already there.
-        if (path.dirname(imagePath) === targetDir)
-            continue
+        .filter(imagePath => path.dirname(imagePath) != targetDir)
 
-        const newImagePath = path.join(targetDir, path.basename(imagePath))
+    console.log(`Found ${imagePathsThatNeedMoving.length} images that need moving`)
 
-        console.log(`Moving image/video from ${imagePath} to ${newImagePath}`)
+    let i = 1
+    for (const oldImagePath of imagePathsThatNeedMoving) {
+        console.log(`${i}/${imagePathsThatNeedMoving.length}`)
 
-        await fs.promises.rename(imagePath, newImagePath)
+        const newImagePath = path.join(targetDir, path.basename(oldImagePath))
+
+        if (auto) {
+            move(oldImagePath, newImagePath)
+        } else {
+            console.log(`Move media file from ${oldImagePath} to ${newImagePath}?`)
+
+            await inquirer
+                .prompt({
+                    name: 'select',
+                    message: `Move media file?`,
+                    type: 'list',
+                    choices: choices,
+                })
+                .then(answers => {
+                    switch(answers.select) {
+                        case choices[0]:
+                            move(oldImagePath, newImagePath)
+                            break;
+                        case choices[1]:
+                            console.log(`NOT moving image/video from ${oldImagePath} to ${newImagePath}`)
+                            break;
+                        case choices[2]:
+                            auto = true
+                            move(oldImagePath, newImagePath)
+                            break;
+                    }   
+                })
+        }
+
+        i++
     }
+
+    console.log(`Finished moving files`)
 }
 
-const moveEm = async function(yamlPaths: string[], targetFolderName: string, filterFunction: Function) {
+const findImagesAndMoveToTarget = async function(yamlPaths: string[], targetFolderName: string, filterFunction: Function) {
     const matchingYamlPaths = yamlPaths
         .filter(x => filterFunction(readYamlFile(x)))
 
@@ -142,13 +196,48 @@ const moveEm = async function(yamlPaths: string[], targetFolderName: string, fil
     await moveImages(matchingImagePaths, targetFolderName)
 }
 
+const imageMoverUi = async function() {
+    const choices = [
+        'Move private images to private folder',
+        'Move archived images to archived folder',
+    ]
+
+    return inquirer
+        .prompt({
+            name: 'select',
+            message: 'Select an option:',
+            type: 'list',
+            choices: choices,
+        })
+        .then(async answers => {
+            const yamlPaths = await recursiveSearchSidecar(sidecarPath)
+            console.log(`Found ${yamlPaths.length} YAML files in ${sidecarPath}`)
+
+            if (answers.select == choices[0]) {
+                await findImagesAndMoveToTarget(yamlPaths, 'private', (file: SidecarFile) => file.Private === true)
+            } else if (answers.select == choices[1]) {
+                await findImagesAndMoveToTarget(yamlPaths, 'archived', (file: SidecarFile) => file.DeletedAt !== undefined)
+            }
+        })
+}
+
 const main = async function() {
-    const yamlPaths = await recursiveSearchSidecar(sidecarPath)
+    inquirer
+        .prompt({
+            name: 'select',
+            message: 'Select an option:',
+            type: 'list',
+            choices: [
+                'Move images',
+            ]
+        })
+        .then(async answers => {
+            if (answers.select == 'Move images') {
+                await imageMoverUi()
+            }
 
-    console.log(`Found ${yamlPaths.length} YAML files in ${sidecarPath}`)
-
-    await moveEm(yamlPaths, 'private', (file: SidecarFile) => file.Private === true)
-    await moveEm(yamlPaths, 'archived', (file: SidecarFile) => file.DeletedAt !== undefined)
+            await main()
+        })
 }
 
 await main()
